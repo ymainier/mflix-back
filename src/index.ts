f8cc2bb6c424a6eb522fc081a8970b1d00ccfc11
Yann Mainier <yann.mainier@gmail.com>
2023-01-22 18:27:45 +0000

    add player control

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import express, { Response } from "express";
 
 const prisma = new PrismaClient();
 const app = express();
 
 function notFound(res: Response): void {
-  res.status(404).json({ error: { code: 404, message: "Not Found" } });
+  res.status(404).json({ errors: [{ code: 404, title: "Not Found" }] });
+}
+
+function url(path: string, qs?: Record<string, string | number>): string {
+  // Encoding the URL properly cause issue with filename containing spaces
+  let _url = `${process.env.VLC_REQUEST_URL}/${path}`;
+  if (qs) {
+    _url += `?${Object.entries(qs)
+      .map(([k, v]) => `${k}=${v}`)
+      .join("&")}`;
+  }
+  return _url;
+}
+
+export async function status(qs: Record<string, string | number> = {}) {
+  // @ts-ignore fetch is still considered experimental in Node 18
+  const result = await fetch(url("status.json", qs));
+  return await result.json();
+}
+
+export async function playlist() {
+  // @ts-ignore fetch is still considered experimental in Node 18
+  const result = await fetch(url("playlist.json"));
+  return await result.json();
 }
 
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
-  const movie = await prisma.movie.findMany();
-  res.json(movie);
+  const movies = await prisma.movie.findMany();
+  res.json({ data: movies });
 });
 
 app.get(`/movies/:id`, async (req, res) => {
@@ -23,12 +46,12 @@ app.get(`/movies/:id`, async (req, res) => {
     notFound(res);
     return;
   }
-  res.json(movie);
+  res.json({ data: movie });
 });
 
 app.get("/shows", async (req, res) => {
   const show = await prisma.show.findMany();
-  res.json(show);
+  res.json({ data: show });
 });
 
 app.get(`/shows/:showId/seasons`, async (req, res) => {
@@ -42,7 +65,7 @@ app.get(`/shows/:showId/seasons`, async (req, res) => {
     notFound(res);
     return;
   }
-  res.json(show.seasons);
+  res.json({ data: show.seasons });
 });
 
 app.get(`/shows/:showId/seasons/:seasonId/episodes`, async (req, res) => {
@@ -58,7 +81,7 @@ app.get(`/shows/:showId/seasons/:seasonId/episodes`, async (req, res) => {
     notFound(res);
     return;
   }
-  res.json(show.seasons[0].episodes);
+  res.json({ data: show.seasons[0].episodes });
 });
 
 app.get(
@@ -79,13 +102,135 @@ app.get(
         },
       },
     });
-    if (show === null || show.seasons.length === 0 || show.seasons[0].episodes.length === 0) {
+    if (
+      show === null ||
+      show.seasons.length === 0 ||
+      show.seasons[0].episodes.length === 0
+    ) {
       notFound(res);
       return;
     }
-    res.json(show.seasons[0].episodes[0]);
+    res.json({ data: show.seasons[0].episodes[0] });
   }
 );
+
+app.post(`/player/play`, async (req, res) => {
+  const { file } = req.query;
+
+  if (typeof file !== "string") {
+    res.status(404).json({
+      errors: [
+        {
+          code: 404,
+          title: "Bad Request",
+          description:
+            "Specify the file to play in a `file` query string parameter",
+        },
+      ],
+    });
+    return;
+  }
+
+  try {
+    await status({ command: "pl_empty" });
+    await status({ command: "in_play", input: file });
+    res.status(200).json({ data: {} });
+    console.log(`Player Play ${file}`);
+  } catch (e) {
+    res.status(500).json({
+      errors: [
+        { status: 500, title: "Internal Server Error", description: `${e}}` },
+      ],
+    });
+  }
+});
+
+app.post(`/player/stop`, async (_req, res) => {
+  try {
+    await status({ command: "pl_play" });
+    await status({ command: "pl_stop" });
+    await status({ command: "pl_empty" });
+    res.status(200).json({ data: {} });
+    console.log(`Player Stop`);
+  } catch (e) {
+    res.status(500).json({
+      errors: [
+        { status: 500, title: "Internal Server Error", description: `${e}}` },
+      ],
+    });
+  }
+});
+
+app.post(`/player/togglePause`, async (_req, res) => {
+  try {
+    await status({ command: "pl_pause" });
+    res.status(200).json({ data: {} });
+    console.log(`Toggle Pause`);
+  } catch (e) {
+    res.status(500).json({
+      errors: [
+        { status: 500, title: "Internal Server Error", description: `${e}}` },
+      ],
+    });
+  }
+});
+
+const NUMBER_REGEXP = /^\d+$/;
+
+app.post(`/player/seek`, async (req, res) => {
+  const { value } = req.query;
+
+  if (typeof value !== "string" || !value.match(NUMBER_REGEXP)) {
+    res.status(404).json({
+      errors: [
+        {
+          code: 404,
+          title: "Bad Request",
+          description:
+            "Specify the value to seek to in `value` query string parameter",
+        },
+      ],
+    });
+    return;
+  }
+  const val = parseInt(value, 10);
+
+  try {
+    await status({ command: "pl_play" });
+    await status({ command: "seek", val  });
+    res.status(200).json({ data: {} });
+    console.log(`Player Seeking to ${val}`);
+  } catch (e) {
+    res.status(500).json({
+      errors: [
+        { status: 500, title: "Internal Server Error", description: `${e}}` },
+      ],
+    });
+  }
+});
+
+app.get(`/player/status`, async (_req, res) => {
+  try {
+    const [ statusData, playlistData ] = await Promise.all([status(), playlist()]);
+    const maybeUri: string | undefined = playlistData?.children?.find(
+      (child: { name?: string}) => child.name === "Playlist"
+    )?.children?.[0]?.uri;
+    const fullpath = typeof maybeUri !== "undefined" ? decodeURI(new URL(maybeUri).pathname) : undefined;
+    res.status(200).json({ data: {
+      status: statusData.state,
+      time: statusData.time,
+      length: statusData.length,
+      fullpath
+    } });
+    console.log(`Player Status`);
+  } catch (e) {
+    res.status(500).json({
+      errors: [
+        { status: 500, title: "Internal Server Error", description: `${e}}` },
+      ],
+    });
+  }
+});
 
 const PORT = 3000;
 const server = app.listen(PORT, () =>
